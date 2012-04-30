a3135be13bde0915dd1bb2c709873d7d204c24fa
Valentin Alexeev <valentin.alekseev@gmail.com>
2012-04-30 17:16:32 +0400

    Update to latest code from GAE. Use better code to detect vk.com video information.

---
+++ static/bookmarklet.local.js
@@ -0,0 +1,58 @@
+function vavpushBookmarklet(){
+  // get the thumbnail from flash player's flashvars
+  var p = document.getElementById('video_player')
+  var fV = p.attributes.flashvars.value;
+  var attrs = fV.split('&');
+
+  var title, thumb, args = [];
+
+  for (var i = 0; i<attrs.length;i++) {
+    var kv = attrs[i].split("=");
+
+    if (kv[0].indexOf("thumb") == 0) {
+        thumb = unescape(attrs[i].split("=")[1]);
+        break;
+    }
+  }
+
+  // get title
+  title = document.getElementById("mv_min_title").innerHTML;
+
+  // prepare quality attributes
+  var f = ["240", "360", "480", "720"];
+
+  for (var i = 3; i > video_max_hd; i--) {
+    f.pop();
+  }
+
+  for (var i=0;i<f.length;i++){
+    args.push("url="+pathToHD(f[i]));
+  }
+  var formats = "q=" + f.join("&q=");
+
+  // prepare the resulting URL
+  args.push(formats);
+  args.push("thumb="+thumb);
+  args.push("title="+encodeURIComponent(title));
+
+  var resultUrl = "http://localhost:8080/submit?" + args.join("&");
+
+  // create a new confirmation card
+  var card = [
+    "<html><head><title>Add video ", title, " to watch queue</title></head>",
+    "<body><img src='", thumb,"'><br/>" , title, "<br/><a href='", resultUrl,"'>add</a></body>"
+  ];
+
+  // show the card
+  document.write(card.join(""));
+};
+
+function pathToHD(res) {
+  var s = (video_host.substr(0, 4) == "http")
+    ? video_host
+    : 'http://cs' + video_host + '.vkontakte.ru/';
+
+  return s + 'u' + video_uid + '/video/' + video_vtag + '.' + res + '.mov';
+}
+
+setTimeout(vavpushBookmarklet, 100);
