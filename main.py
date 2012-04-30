61d8b0267ee86095160b9e6e136d56e703038bc0
Valentin Alexeev <valentin.alekseev@gmail.com>
2012-04-30 17:10:56 +0400

    Update to latest code from GAE. Major update to support multiply TV sets.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 #
+# Copyright 2011,2012 Valentin Alexeev
 # Copyright 2007 Google Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -18,96 +19,163 @@ from google.appengine.ext import webapp
 from google.appengine.api import users
 from google.appengine.ext.webapp import util
 from google.appengine.ext.webapp import template
-from google.appengine.ext import db
+from google.appengine.api import urlfetch
+
 import cgi
 import os
 import urllib2
+from urlparse import urlsplit
 import logging
-                                      
+import httplib
+
+from datamodel import *
+
 class MainHandler(webapp.RequestHandler):
+	def get(self):
+		if 'tvcookie' in self.request.cookies:
+			tvcookie = int(cgi.escape(self.request.cookies['tvcookie']))
+			#videos = db.GqlQuery("SELECT * FROM VideoItem WHERE tvcookie = :1 ORDER BY title", tvcookie)
+			videos = db.GqlQuery("SELECT * FROM VideoItem WHERE tvcookie = :1", tvcookie)
+			template_values = {
+				'videos': videos,
+				'logout': users.create_logout_url('/')
+			}
+			path = os.path.join(os.path.dirname(__file__), 'templates/list.html')
+			self.response.out.write(template.render(path, template_values))
+		else:
+			self.redirect('/static/tvlogin.html')
+
+class VideoItemHandler(webapp.RequestHandler):
     def get(self):
         user = users.get_current_user()
 
         if user:
-            videos = db.GqlQuery("SELECT * FROM VideoSubmit WHERE author = :1", user)
-            template_values = {
-                'videos': videos,
-                'logout': users.create_logout_url('/')
-            }
-            path = os.path.join(os.path.dirname(__file__), 'templates/list.html')
-            self.response.out.write(template.render(path, template_values))
-        
-        else:
-            self.redirect(users.create_login_url(self.request.uri))
-
-class VideoSubmitHandler(webapp.RequestHandler):
-    def get(self):
-        user = users.get_current_user()
-
-        if user:
-            urls = self.request.get_all('url')
-
-            for url in urls:
-                logging.info("Adding URL %s for %s", url, user)
-                item = VideoSubmit()
-                item.author = user
-                item.url = cgi.escape(url)
-                item.title = cgi.escape(self.request.get('title'))
-                item.thumb = cgi.escape(self.request.get('thumb'))
-                item.put()
-#                request = urllib2.Request(url)
-#                request.get_method = lambda: 'HEAD'
-#                try:
-#                    response = urllib2.urlopen(request)
-#                    logging.info("Adding URL %s for %s", url, user)
-
-#                    item = VideoSubmit()
-#                    item.author = user
-#                    item.url = cgi.escape(url)
-#                    item.title = cgi.escape(self.request.get('title'))
-#                    item.thumb = cgi.escape(self.request.get('thumb'))
-#                    item.put()
-#                except urllib2.HTTPError, e:
-#                    logging.info("Failed to add URL %s for %s code %i", url, user, e.code)
-#                    self.response.out.write(e.code);
-            self.redirect('/')
+            if 'tvcookie' in self.request.cookies:
+				tvcookie = int(cgi.escape(self.request.cookies['tvcookie']))
+				q = db.GqlQuery('SELECT * FROM TvAccess WHERE user = :1 AND tvcookie = :2', user, tvcookie)
+												
+				if q.get():
+					logging.info('User %s access ok to tv %s', user, tvcookie)
+					
+					item = VideoItem()
+					item.tvcookie = tvcookie
+					item.title = cgi.escape(self.request.get('title'))
+					item.thumb = cgi.escape(self.request.get('thumb'))
+					item.put()
+					
+					urlsL = self.request.get_all('url')
+					qualitiesL = self.request.get_all('q')
+					
+					for pair in zip(urlsL, qualitiesL):
+						logging.info("Adding URL %s for %s", pair[0], tvcookie)
+						itemurl = VideoItemUrl()
+						itemurl.videoitem = item
+						itemurl.url = cgi.escape(pair[0])
+						itemurl.quality = cgi.escape(pair[1])
+						itemurl.put()
+					self.redirect('/')
+				else:
+					logging.info('User %s access failed to tv %s', user, tvcookie)
+					self.redirect('/static/error.noaccess.html')
+            else:
+                self.redirect('/static/tvlogin.html')
         else:
             self.redirect(users.create_login_url(self.request.uri))
 
 class VideoPlayerHandler(webapp.RequestHandler):
     def get(self):
-        template_values = {
-            'url': cgi.escape(self.request.get('url'))
-        }
-        path = os.path.join(os.path.dirname(__file__), 'templates/player.html')
-        self.response.out.write(template.render(path, template_values))
+		key = cgi.escape(self.request.get('key'))
+		quality = cgi.escape(self.request.get('quality'))
+		vi = None
+		
+		try:
+			vi = VideoItem.get(key)
+		except:
+			self.redirect('/static/error.nosuchvideo.html')
+			return
+		
+		urlItem = vi.urls.filter("quality = ", quality).get()
+		
+		if not urlItem:
+			self.redirect('/static/error.nosuchquality.html')
+			return
+		
+		template_values = {
+			'thumb': vi.thumb,
+			'offset': vi.offset,
+			'url': urlItem.url,
+			'key': key
+		}
+		
+		player = "templates/player-object.html";
+		
+		if "SmartTV" in self.request.headers['User-Agent']:
+			# let's try to use <video> player
+			player = "templates/player-video.html";
+		
+		path = os.path.join(os.path.dirname(__file__), player)
+		self.response.out.write(template.render(path, template_values))
 
 class VideoDeleteHandler(webapp.RequestHandler):
     def get(self):
-        user = users.get_current_user()
-
-        if user:
-            videos = db.GqlQuery("SELECT * FROM VideoSubmit WHERE author = :1 AND url = :2", user, cgi.escape(self.request.get('url')))
-            for video in videos:
-                video.delete()
-            self.redirect('/')
-        
+        if 'tvcookie' in self.request.cookies:
+			item = VideoItem.get(cgi.escape(self.request.get('key')))
+			for url in item.urls:
+				url.delete()
+			item.delete()
+			self.redirect('/')
         else:
-            self.redirect(users.create_login_url(self.request.uri))
+            self.redirect('/static/tvlogin.html')
 
-class VideoSubmit(db.Model):
-    author = db.UserProperty()
-    url = db.StringProperty()
-    title = db.StringProperty()
-    thumb = db.StringProperty()
+class VideoPauseHandler(webapp.RequestHandler):
+	def get(self):
+		if 'tvcookie' in self.request.cookies:
+			item = VideoItem.get(cgi.escape(self.request.get('key')))
+			item.offset = cgi.escape(self.request.get('offset'))
+			item.put()
+			self.redirect('/')
+		else:
+			self.redirect('/static/tvlogin.html')
+
+class TvLoginHandler(webapp.RequestHandler):
+    def get(self):
+        self.response.headers.add_header('Set-Cookie', 'tvcookie=' + cgi.escape(self.request.get('tvcookie')) + ";path=/")
+        self.redirect('/')
+
+class TvLogoutHandler(webapp.RequestHandler):
+    def get(self):
+        self.response.headers.add_header('Set-Cookie', 'tvcookie=;Max-Age=0;path=/')
+        self.redirect(users.create_logout_url('/'))
+
+class TvRegisterHandler(webapp.RequestHandler):
+	def get(self):
+		user = users.get_current_user()
+		
+		if user:	
+			if users.is_current_user_admin():
+				access = TvAccess();
+				access.tvcookie = int(cgi.escape(self.request.get('tvcookie')))
+				access.user = users.User(cgi.escape(self.request.get('userid')))
+				access.put()
+				self.redirect('/static/tvregister.html')
+			else:
+				self.redirect('/static/error.unauthorized.html')
+		else:
+			self.redirect(users.create_login_url(self.request.uri))	
+
+# Entry point
 
 def main():
     logging.getLogger().setLevel(logging.DEBUG)
     application = webapp.WSGIApplication([
         ('/', MainHandler),
-        ('/submit', VideoSubmitHandler),
+        ('/submit', VideoItemHandler),
         ('/delete', VideoDeleteHandler),
-        ('/player', VideoPlayerHandler)
+        ('/player', VideoPlayerHandler),
+        ('/tvlogin', TvLoginHandler),
+		('/tvlogout', TvLogoutHandler),
+		('/tvregister', TvRegisterHandler),
+		('/pause', VideoPauseHandler)
         ],debug=True)
     util.run_wsgi_app(application)
 
